292606664d3cacaa8b0f89ff5615431144e89483
Benjamin Verbeek <benjaminverbeek79@gmail.com>
2021-03-30 12:07:59 +0200

    changed message in testProgram

--- testProgram.py
+++ testProgram.py
@@ -1,5 +1,15 @@
 # Benjamin Verbeek
 # 2021-03-20
 
-print("Hello GitHub!")
-print("Changing stuff...")
+print("Hello Android!")
+print("Changing NOTHING")
+
+print("Nothing to see here...")
+
+
+
+
+
+
+
+# or is there?
